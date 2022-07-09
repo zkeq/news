fb878b63df0b9bac0257fcbffe70d71e3dcb676e
valetzx <45054400+valetzx@users.noreply.github.com>
2022-07-09 13:02:25 +0000

    replit support

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# 每日早报
+# news-on-replit ！
 
 https://news.icodeq.com
+
+https://news.pigp.repl.co
+
+本分支可在replit一键部署，并且前后端分离。
+
+但是效率比不上vercel服务，以下是一键部署脚本：
+
+```
+git clone https://github.com/valetzx/newsonreplit && mv -b newsonreplit/* ./ && mv -b newsonreplit/.[^.]* ./ && rm -rf *~ && rm -rf newsonreplit
+```
+
+再次感谢zkeq！
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # coding:utf-8
 import uvicorn
 from fastapi import FastAPI, Response
-from api.crawler import main as new
+from starlette.responses import FileResponse 
+from crawler import main as new
 
 app = FastAPI()
+
+@app.get("/")
+async def read_index():
+    return FileResponse('index.html')
+
+@app.get("/index.js")
+async def read_js():
+    return FileResponse('index.js')
+
+@app.get("/news.css")
+async def read_css():
+    return FileResponse('news.css')
 
 @app.get("/api")
 def news(response: Response, index: int = 0, origin: str = 'zhihu', cache: str = 'null'):
@@ -16,4 +29,4 @@ def news(response: Response, index: int = 0, origin: str = 'zhihu', cache: str =
 
 
 if __name__ == "__main__":
-    uvicorn.run("fast:app", host="127.0.0.1", port=61, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
