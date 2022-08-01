493d7d309ada2195442723060f3e0a68a759134c
zkeq <admin@icodeq.com>
2022-08-01 13:23:09 +0800

    微语 have 逗号

--- api/crawler.py
+++ api/crawler.py
@@ -47,7 +47,7 @@ def get_163_days(index):
     for i in list_all:
         if "<" not in i and ">" not in i and i != '':
             i.replace('\u200b', '')
-            if '、' in i:
+            if '、' in i and "微语" not in i:
                 new_str = '、'.join(i.split('、')[1:])
                 news_list.append(new_str)
             final_list.append(i)
